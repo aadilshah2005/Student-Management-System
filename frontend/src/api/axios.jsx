39269b147c97db43ef0bb5d5d6c48755c252a34b
aadilshah2005 <154650179+aadilshah2005@users.noreply.github.com>
2025-10-31 10:01:50 +0530

    Update API base URL for development and production

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/v1",   // proxy ke liye short path
-  withCredentials: true, // cookies ke liye important
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api/v1" // local backend URL
+      : "https://student-management-system-5yu7.onrender.com/api/v1", // Render backend URL
+  withCredentials: true, // cookies ke liye zaruri
 });
 
 export default api;
